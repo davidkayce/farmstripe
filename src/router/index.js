1a2f787faf5d842bac6f8d3cbb31e9286e77af9b
davidkayce <david@kora.network>
2019-05-22 12:36:39 +0100

    update: removed scrolling

--- src/router/index.js
+++ src/router/index.js
@@ -6,15 +6,7 @@ Vue.use(Router)
 export default new Router({
   saveScrollPosition: true,
   mode: 'history',
-  scrollBehavior (to, from, savedPosition) {
-    if (savedPosition) {
-      return savedPosition
-    }
-    if (to.hash) {
-      return {selector: to.hash}
-    }
-    return {x: 0, y: 0}
-  },
+
   routes: [
 
     {
