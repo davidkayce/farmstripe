a6c7dc38d324f85be493ed0417ad3c21d0f70175
davidkayce <david@kora.network>
2019-06-26 14:48:20 +0100

    feat: added SEO features

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
+const path = require('path') 
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+
 module.exports = {
   css: {
     loaderOptions: {
@@ -5,5 +8,14 @@ module.exports = {
         data: `@import "@/assets/scss/main.scss";`
       }
     }
+  },
+  configureWebpack: {
+    plugins: [
+      new PrerenderSPAPlugin({
+        staticDir: path.join(__dirname, 'dist'),
+        // Required - Routes to render.
+        routes: [ '/', '/contact', '/privacy' ],
+      })
+    ]
   }
 };
